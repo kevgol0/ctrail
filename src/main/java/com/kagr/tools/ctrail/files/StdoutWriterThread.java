8b5f23f427922e9c40539ee9db8af894313c13be
Kevin A. Goldstein R <kevin@neeveresearch.com>
2019-11-21 22:35:07 -0500

    remoed uncessary resources

---
+++ src/main/java/com/kagr/tools/ctrail/files/StdoutWriterThread.java
@@ -0,0 +1,68 @@
+/****************************************************************************
+ * FILE: CtrailWriterThread.java
+ * DSCRPT: 
+ ****************************************************************************/
+
+
+
+
+
+package com.kagr.tools.ctrail.files;
+
+
+
+
+
+import java.util.concurrent.BlockingDeque;
+
+
+
+import com.kagr.tools.ctrail.StringFormatter;
+
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+
+
+public class StdoutWriterThread implements Runnable
+{
+	@Getter @Setter(AccessLevel.PRIVATE) BlockingDeque<String> _output;
+	@Getter StringFormatter _formatter;
+
+
+
+
+
+	public StdoutWriterThread(BlockingDeque<String> output_)
+	{
+		setOutput(output_);
+		_formatter = new StringFormatter();
+	}
+
+
+
+
+
+	@Override
+	public void run()
+	{
+		while (true)
+		{
+			try
+			{
+				System.out.println(_formatter.format(_output.take()));
+			}
+			catch (InterruptedException ex_)
+			{
+				ex_.printStackTrace(System.err);
+			}
+		}
+	}
+
+}
